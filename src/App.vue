e8e6fe3d14ec4741d11ac8d1ba9764b3d6fbefad
AdzhanSerik <adzhan.serik@gmail.com>
2025-04-10 19:52:42 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="px-4">
+    <Header :navigation="navigation" />
+  </div>
+</template>
+
+<script setup>
+import Header from "./components/Header.vue";
+
+const navigation = [
+  'Home',
+  'Categories',
+  'My Collections',
+  'Blog',
+]
+
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="logo text-white">
+            <img src="../assets/logo.svg" alt="">
+            <span>antools.</span>
+        </div>
+        <nav>
+            <Navigation v-for="(nav, index) in navigation" :key="index" :nav="nav" />
+        </nav>
+
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    navigation: Array,
+})
+import Navigation from "./Navigation.vue";
+
+</script>
+
+<style lang="scss" scoped></style>
